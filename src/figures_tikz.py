16027d91caa1b20bd73516e2c447c472b73a33fb
Jacob Crainic <jacobcrainic2008@gmail.com>
2026-08-29 03:26:22 -0400

    3.3 paraphrase in author voice, intro enumeration harmonized

--- src/figures_tikz.py
+++ src/figures_tikz.py
@@ -70,7 +70,7 @@ def fig_one_curve(panel):
     mean_e = cells.loc[cells["eligible"], "share_newer"].mean()
     mean_p = cells.loc[cells["placebo"], "share_newer"].mean()
 
-    axis = ("causalre wide, height=2.6cm, xmin=0, xmax=1, xtick={0,0.5,1}, "
+    axis = ("causalre wide, height=2.45cm, xmin=0, xmax=1, xtick={0,0.5,1}, "
             "xlabel={share of window newer than the focal model}")
     return f"""\\begin{{tikzpicture}}
   \\begin{{axis}}[{axis}, ymin=20, ymax=80,
@@ -123,10 +123,10 @@ def fig_boundary(panel):
         parts[side + "_share"] = day_binned(sub, "share_newer")
         parts[side + "_pct"] = day_binned(sub, "percentile")
 
-    top = ("causalre wide, height=2.25cm, xmin=-360, xmax=720, "
+    top = ("causalre wide, height=2.15cm, xmin=-360, xmax=720, "
            "xtick={-360,0,360,720}, xticklabels={,,,}, "
            "ylabel={share}, ylabel style={font=\\scriptsize}, ymin=0.25, ymax=0.95, ytick={0.3,0.6,0.9}")
-    bot = ("causalre wide, height=2.25cm, xmin=-360, xmax=720, "
+    bot = ("causalre wide, height=2.15cm, xmin=-360, xmax=720, "
            "xtick={-360,0,360,720}, "
            "xlabel={benchmark maturity at release (days)}, "
            "ylabel={standing}, ylabel style={font=\\scriptsize}, ymin=30, ymax=75")
